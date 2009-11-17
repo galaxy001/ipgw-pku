cda1fcb5581ae3feba75eb943426554aef4cfcbf
chenxing <chenxing@e88656df-e3d4-5f21-d6dd-225733394d03>
2009-11-17 01:51:20 +0000

    加入不保存密码、不自动连接功能

--- ipgw/install.sh
+++ ipgw/install.sh
@@ -31,83 +31,93 @@ echo "Please contact Chen Xing ( cxcxcxcx@gmail.com ) to report bugs."
 
 echo "Uninstalling..."
 ./uninstall.sh
-echo "If you are using NetworkManager with the dispatcher enabled (Ubuntu do this by default, for example), please choose the first one."
-
-PS3="Please select your distribution: "
-select DISTR in NetworkManager-with-Dispatcher Debian/Ubuntu Gentoo openSUSE Archlinux Other
-do
-	case $DISTR in
-		NetworkManager-with-Dispatcher )
-			DISTR="nm"
-			break
-			;;
-		Debian/Ubuntu )
-			DISTR="DEBLIKE"
-			break
-			;;
-		Gentoo )
-			DISTR="GENTOO"
-			break
-			;;
-		openSUSE )
-			DISTR="SUSE"
-			break
-			;;
-		Archlinux )
-			DISTR="ARCH"
-			break
-			;;
-		Other )
-			DISTR="FEDORA"
-			break
-			;;
-		*)
-			echo "Please give a correct choice!"
-			;;
-	esac
-done
-
-# ∂¡»Î√‹¬Î
-echo "Please input your ipgw username and password."
-read -p "Username: " USERNAME
-read -r -s -p "Password: " PASSWORD
-
-PASSWORD=`python ./ipgw mkPwdStr <<< "$PASSWORD"`
-
 
 install -m 755 -o root -g root ipgw /bin/ipgw
-sed -i "s/YourUserName/$USERNAME/" /bin/ipgw
-sed -i "s/YourPassword/$PASSWORD/" /bin/ipgw
-if [ $DISTR = "DEBLIKE" ]; then
-	install -m 755 -o root -g root ipgws /etc/init.d/ipgw
-	echo -ne '#!/bin/bash\nipgw connect\n' > /etc/network/if-up.d/ipgw
-	echo -ne '#!/bin/bash\nipgw disconnect\n' > /etc/network/if-down.d/ipgw
-	chmod +x /etc/network/if-up.d/ipgw
-	chmod +x /etc/network/if-down.d/ipgw
-	#ln -fs /etc/init.d/ipgw /etc/rc2.d/S90ipgw
-elif [ $DISTR = "nm" ]; then
-	install -m 755 -o root -g root ipgw_nm /etc/NetworkManager/dispatcher.d/99_ipgw
-elif [ $DISTR = "FEDORA" ]; then
-	install -m 755 -o root -g root ipgws /etc/init.d/ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc5.d/S90ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc6.d/K13ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc0.d/K13ipgw
-elif [ $DISTR = "GENTOO" ]; then
-	install -m 755 -o root -g root ipgws_gen /etc/init.d/ipgw
-	rc-update add ipgw default
-elif [ $DISTR = "SUSE" ]; then
-	install -m 755 -o root -g root ipgws /etc/init.d/ipgw
-	ln -fs /etc/init.d/ipgw /etc/init.d/rc5.d/S90ipgw
-	ln -fs /etc/init.d/ipgw /etc/init.d/rc6.d/K13ipgw
-	ln -fs /etc/init.d/ipgw /etc/init.d/rc0.d/K13ipgw
-elif [ $DISTR = "ARCH" ]; then
-	install -m 755 -o root -g root ipgws_arch /etc/rc.d/ipgw
-else
-	install -m 755 -o root -g root ipgws /etc/init.d/ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc5.d/S90ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc6.d/K13ipgw
-	ln -fs /etc/init.d/ipgw /etc/rc0.d/K13ipgw
+echo -ne "\033[0;35mSave password\033[0m (or ipgw can't run automatically)? [Y/n]"
+read ans
+if [ "$ans" == "y" ] || [ "$ans" == "Y" ] || [ "$ans" == "" ]; then
+	# ∂¡»Î√‹¬Î
+	echo "Please input your ipgw username and password."
+	read -p "Username: " USERNAME
+	read -r -s -p "Password: " PASSWORD
+	echo
+
+	PASSWORD=`python ./ipgw mkPwdStr <<< "$PASSWORD"`
+	sed -i "s/YourUserName/$USERNAME/" /bin/ipgw
+	sed -i "s/YourPassword/$PASSWORD/" /bin/ipgw
+
+	echo -ne "\033[0;35mRun ipgw automatically whenever network is ready?\033[0m  [Y/n]"
+	read ans
+	if [ "$ans" == "y" ] || [ "$ans" == "Y" ] || [ "$ans" == "" ]; then
+		echo "If you are using NetworkManager with the dispatcher enabled (Ubuntu do this by default, for example), please choose the first one."
+
+		PS3="Please select your distribution: "
+		select DISTR in NetworkManager-with-Dispatcher Debian/Ubuntu Gentoo openSUSE Archlinux Other
+		do
+			case $DISTR in
+				NetworkManager-with-Dispatcher )
+					DISTR="nm"
+					break
+					;;
+				Debian/Ubuntu )
+					DISTR="DEBLIKE"
+					break
+					;;
+				Gentoo )
+					DISTR="GENTOO"
+					break
+					;;
+				openSUSE )
+					DISTR="SUSE"
+					break
+					;;
+				Archlinux )
+					DISTR="ARCH"
+					break
+					;;
+				Other )
+					DISTR="FEDORA"
+					break
+					;;
+				*)
+					echo "Please give a correct choice!"
+					;;
+			esac
+		done
+		if [ $DISTR = "DEBLIKE" ]; then
+			install -m 755 -o root -g root ipgws /etc/init.d/ipgw
+			echo -ne '#!/bin/bash\nipgw connect\n' > /etc/network/if-up.d/ipgw
+			echo -ne '#!/bin/bash\nipgw disconnect\n' > /etc/network/if-down.d/ipgw
+			chmod +x /etc/network/if-up.d/ipgw
+			chmod +x /etc/network/if-down.d/ipgw
+			#ln -fs /etc/init.d/ipgw /etc/rc2.d/S90ipgw
+		elif [ $DISTR = "nm" ]; then
+			install -m 755 -o root -g root ipgw_nm /etc/NetworkManager/dispatcher.d/99_ipgw
+		elif [ $DISTR = "FEDORA" ]; then
+			install -m 755 -o root -g root ipgws /etc/init.d/ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc5.d/S90ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc6.d/K13ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc0.d/K13ipgw
+		elif [ $DISTR = "GENTOO" ]; then
+			install -m 755 -o root -g root ipgws_gen /etc/init.d/ipgw
+			rc-update add ipgw default
+		elif [ $DISTR = "SUSE" ]; then
+			install -m 755 -o root -g root ipgws /etc/init.d/ipgw
+			ln -fs /etc/init.d/ipgw /etc/init.d/rc5.d/S90ipgw
+			ln -fs /etc/init.d/ipgw /etc/init.d/rc6.d/K13ipgw
+			ln -fs /etc/init.d/ipgw /etc/init.d/rc0.d/K13ipgw
+		elif [ $DISTR = "ARCH" ]; then
+			install -m 755 -o root -g root ipgws_arch /etc/rc.d/ipgw
+		else
+			install -m 755 -o root -g root ipgws /etc/init.d/ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc5.d/S90ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc6.d/K13ipgw
+			ln -fs /etc/init.d/ipgw /etc/rc0.d/K13ipgw
+		fi
+	fi
 fi
+
+
 
 echo
 echo "OK."
